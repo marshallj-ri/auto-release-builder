61c0fe7fc23c0f97d5b8f81d8f8cadba5b581226
Jon Marshall <jmarshall@reapit.com>
2020-03-12 14:08:52 +0000

    Amended version increment logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,17 +48,15 @@ get_version_from_tag()
 increment_version ()
 {
   declare -a part=( ${1//\./ } )
-  declare    new
+  declare -i   new
   declare -i carry=1
 
-  for (( CNTR=${#part[@]}-2; CNTR>=0; CNTR-=1 )); do
-    len=${#part[CNTR]}
-    new=$((part[CNTR]+carry))
-    [ ${#new} -gt $len ] && carry=1 || carry=0
-    [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
-  done
-  new="${part[*]}"
-  echo -e "${new// /.}"
+	new=${part[1]}+1
+	part[1]=$new
+	part[2]=0
+
+  new_version=${part[*]}
+  echo "${new_version// /.}"
 } 
 
 # ==================== MAIN ====================
